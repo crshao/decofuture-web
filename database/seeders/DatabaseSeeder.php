220050785dc54356afc9380cdcd01e5931bedaf8
arufiand <alfiandannyarmanta@gmail.com>
2021-08-21 20:32:31 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(ProductSeeder::class);
         $this->call(SellerSeeder::class);
+        $this->call(userSeeder::class);
     }
 }
---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(
+            [
+                'name' => 'Administrator',
+            ],
+        );
+        role::create(
+            [
+                'name' => 'operatorForklift',
+            ],
+        );
+        User::create([
+            "name" => "Qhurma",
+            "email" => "Qhurma@gmail.com",
+            "phone_number" => "085733296961",
+            "password" => Hash::make("12345"),
+            "role" => 1,
+        ]);
+    }
+}
